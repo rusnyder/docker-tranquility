9d30d069dcbc3df36202c32e42616068f5a2f448
Russell Snyder <russell@redowlanalytics.com>
2017-07-27 11:22:37 -0400

    Added ability to provide custom config file
    Signed-off-by: Russell Snyder <russell@redowlanalytics.com>

---
+++ README.md
@@ -0,0 +1,61 @@
+Docker Tranquility
+==================
+
+Tags:
+
+- latest ([Dockerfile](https://github.com/rusnyder/docker-tranquility/blob/master/Dockerfile))
+
+
+What is Tranqulity?
+===================
+
+From the [Tranquility Github page](https://github.com/druid-io/tranquility):
+
+> Tranquility helps you send real-time event streams to Druid and handles partitioning, replication, service discovery, and schema rollover, seamlessly and without downtime.
+
+How to use?
+===========
+
+This is best used as part of a docker-compose.yml file with Druid also being
+created.  By default, the container launches the Tranquility server with no
+pre-loaded configs, but you can also provide a config file by mounting one
+and specifying its location via the `TRANQUILITY_CONFIG_FILE` environment
+variable.
+
+So assuming you have a config file located at `conf/tranquility/config.json` in your
+local working directory and a full druid stack + zookeeper running on your
+local machine (or in containers w/ exposed ports), you could spin up tranquility
+with the following:
+
+```shell
+docker run -it \
+  --network host \
+  -v `pwd`/conf/tranquility:/etc/tranquility \
+  -e TRANQUILITY_CONFIG_FILE=/etc/tranquility/config.json \
+  -e TRANQUILITY_ZOOKEEPER_CONNECT=localhost:2181 \
+  -p 8200:8200 \
+  rusnyder/tranquility
+```
+
+Alternately, you could add this as a service in a docker-compose file as follows:
+
+```yaml
+version: "2"
+services:
+  zookeeper:
+    image: confluentinc/cp-zookeeper:latest
+    environment:
+      ZOOKEEPER_CLIENT_PORT: 2181
+      ZOOKEEPER_TICK_TIME: 2000
+    ports:
+      - 2181:2181
+  tranquility:
+    image: rusnyder/tranquility:latest
+    volumes:
+      - ./conf/tranquility:/etc/tranquility
+    environment:
+      TRANQUILITY_CONFIG_FILE: /etc/tranquility/config.json
+      TRANQUILITY_PROPERTIES_ZOOKEEPER_CONNECT: zookeeper:2181
+    ports:
+      - 8200:8200
+```
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,32 +1,41 @@
 #!/bin/sh
 
+set -eo pipefail
+
 # Vars
 INSTALL_DIR=/usr/share/tranquility
-CONFIG_FILE=${INSTALL_DIR}/conf/tranquility/server.json
+CONF=''
 
-# Process any env vars
-jq_exp=''
-for var in `env | grep -E '^TRANQUILITY_PROPERTIES_'`; do
+# If a config file was specified, we'll start the server with
+# that configuration pre-loaded
+if [ -n "$TRANQUILITY_CONFIG_FILE" ]; then
+  # Additional env vars prefixed with "TRANQUILITY_PROPERTIES_"
+  # can be used to override connection properties from the provided
+  # config file
+  jq_exp=''
+  for var in `env | grep -E '^TRANQUILITY_PROPERTIES_'`; do
+    if [ -n "$jq_exp" ]; then
+      jq_exp="${jq_exp} | "
+    fi
+    key=`echo "$var" \
+      | cut -d'=' -f1 \
+      | sed -e 's/TRANQUILITY_PROPERTIES_//' \
+      | tr '[:upper:]' '[:lower:]' \
+      | tr '_' '.'`
+    val=`echo "$var" | sed -e 's/^[^=]\+=//'`
+    jq_exp="${jq_exp}.properties[\"$key\"] = \"$val\""
+  done
+
+  # If we created a substitution string, apply it to the config
   if [ -n "$jq_exp" ]; then
-    jq_exp="${jq_exp} | "
+    tmpf=$(mktemp)
+    jq "$jq_exp" $TRANQUILITY_CONFIG_FILE > "$tmpf"
+    TRANQUILITY_CONFIG_FILE="$tmpf"
   fi
-  key=`echo "$var" \
-    | cut -d'=' -f1 \
-    | sed -e 's/TRANQUILITY_PROPERTIES_//' \
-    | tr '[:upper:]' '[:lower:]' \
-    | tr '_' '.'`
-  val=`echo "$var" | sed -e 's/^[^=]\+=//'`
-  jq_exp="${jq_exp}.properties[\"$key\"] = \"$val\""
-done
 
-echo "VAR: $jq_exp"
-
-# If we created a substitution string, apply it to the config
-if [ -n "$jq_exp" ]; then
-  tmpf=$(mktemp)
-  jq "$jq_exp" $CONFIG_FILE > "$tmpf"
-  mv -f "$tmpf" "$CONFIG_FILE"
+  # Finally, define the config string to append to the executable
+  CONF="-configFile $TRANQUILITY_CONFIG_FILE"
+  echo "Running with config: `cat $TRANQUILITY_CONFIG_FILE | jq --compact-output .`"
 fi
 
-cat $CONFIG_FILE
-#echo ${INSTALL_DIR}/bin/tranquility server -configFile ${CONFIG_FILE}
+${INSTALL_DIR}/bin/tranquility server ${CONF}
