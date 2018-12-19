2a67b850fada77f4b171557382caeb01d1f75efa
Russell Snyder <ru.snyder@gmail.com>
2018-12-19 18:13:52 -0500

    Fixing shellcheck errors
    Signed-off-by: Russell Snyder <ru.snyder@gmail.com>

--- start-tranquility.sh
+++ start-tranquility.sh
@@ -13,29 +13,29 @@ if [ -n "$TRANQUILITY_CONFIG_FILE" ]; then
   # can be used to override connection properties from the provided
   # config file
   jq_exp=''
-  for var in `env | grep -E '^TRANQUILITY_PROPERTIES_'`; do
+  for var in $(env | grep -E '^TRANQUILITY_PROPERTIES_'); do
     if [ -n "$jq_exp" ]; then
       jq_exp="${jq_exp} | "
     fi
-    key=`echo "$var" \
+    key="$(echo "$var" \
       | cut -d'=' -f1 \
       | sed -e 's/TRANQUILITY_PROPERTIES_//' \
       | tr '[:upper:]' '[:lower:]' \
-      | tr '_' '.'`
-    val=`echo "$var" | sed -e 's/^[^=]\+=//'`
+      | tr '_' '.')"
+    val="$(echo "$var" | sed -e 's/^[^=]\+=//')"
     jq_exp="${jq_exp}.properties[\"$key\"] = \"$val\""
   done
 
   # If we created a substitution string, apply it to the config
   if [ -n "$jq_exp" ]; then
     tmpf=$(mktemp)
-    jq "$jq_exp" $TRANQUILITY_CONFIG_FILE > "$tmpf"
+    jq "$jq_exp" "$TRANQUILITY_CONFIG_FILE" > "$tmpf"
     TRANQUILITY_CONFIG_FILE="$tmpf"
   fi
 
   # Finally, define the config string to append to the executable
   CONF="-configFile $TRANQUILITY_CONFIG_FILE"
-  echo "Running with config: `cat $TRANQUILITY_CONFIG_FILE | jq --compact-output .`"
+  echo "Running with config: $(jq --compact-output . < "$TRANQUILITY_CONFIG_FILE")"
 fi
 
-${INSTALL_DIR}/bin/tranquility server ${CONF}
+"${INSTALL_DIR}/bin/tranquility" server "$CONF"
